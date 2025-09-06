ffa36278a9951f2f0b1e80cf36e4f9ad0fd7d16c
SNGSIY006 <SNGSIY006@myuct.ac.za>
2025-09-06 12:08:15 +0200

    Project file initial commit

---
+++ src/main/java/com/numbersummarizer/numbersummarizer/NumbersummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.numbersummarizer.numbersummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numbersummarizer/numbersummarizer/NumbersummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numbersummarizer.numbersummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
