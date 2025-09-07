5772779e7e95f9f3e5b44dc8f3a7b463fd97a55f
SNGSIY006 <SNGSIY006@myuct.ac.za>
2025-09-07 15:11:07 +0200

    Added spring context class , Logger, and Initialised Number Range and Settings

--- src/main/java/com/numbersummarizer/numbersummarizer/NumbersummarizerApplication.java
+++ src/main/java/com/numbersummarizer/numbersummarizer/NumbersummarizerApplication.java
@@ -1,13 +1,35 @@
 package com.numbersummarizer.numbersummarizer;
 
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.numbersummarizer.numbersummarizer.Infrastructure.settings.InputSetting;
+import com.numbersummarizer.numbersummarizer.common.NumberRange;
 
 @SpringBootApplication
 public class NumbersummarizerApplication {
-
+	private static Logger log = LoggerFactory.getLogger(NumbersummarizerApplication.class);
+	
 	public static void main(String[] args) {
-		SpringApplication.run(NumbersummarizerApplication.class, args);
+		
+		ApplicationContext context = SpringApplication.run(NumbersummarizerApplication.class, args);
+		NumberRange numberRange = context.getBean(NumberRange.class);
+		InputSetting inputSetting = new InputSetting();
+		Collection<Integer> collected = numberRange.collect();
+		inputSetting.setSequenceString("1,2,2,2,3,3,3,5,5,6,7,10,11,12,13,14,15,16,20,21,22,23");
+		numberRange.setInput(inputSetting);
+		Collection<Integer> collection2 = numberRange.collect();
+		log.info("================================================================================================================================================================");
+		log.info("Summarised Range: {}" , numberRange.summarizeCollection(collected));
+		log.info("Summarised Range: {}" , numberRange.summarizeCollection(collection2));
+		log.info("================================================================================================================================================================");
+
+
 	}
 
 }
