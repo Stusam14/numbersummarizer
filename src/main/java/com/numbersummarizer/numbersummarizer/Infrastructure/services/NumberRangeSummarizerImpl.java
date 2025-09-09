7a7f7ee7ee28bd9345e7ead07fb7c9bd3955c789
SNGSIY006 <SNGSIY006@myuct.ac.za>
2025-09-09 22:31:08 +0200

    Added an optional nul check for the string

--- src/main/java/com/numbersummarizer/numbersummarizer/Infrastructure/services/NumberRangeSummarizerImpl.java
+++ src/main/java/com/numbersummarizer/numbersummarizer/Infrastructure/services/NumberRangeSummarizerImpl.java
@@ -1,8 +1,11 @@
 package com.numbersummarizer.numbersummarizer.Infrastructure.services;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,26 +19,37 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
     @Override
     public Collection<Integer> collect(String input) {
 
+        Optional<String> str = Optional.ofNullable(input);
+        String strValue = str.orElseThrow(()-> new NullPointerException("Input cannot be null"));
+        if(strValue.isEmpty()) throw new IllegalArgumentException("input cannot be empty!");
+
         return  Arrays
                 .stream(input.split(","))
                 .map(String::trim)
+                .filter(num->num.chars()
+                .allMatch(Character::isDigit))
                 .map(Integer::parseInt)
+                .distinct()
+                .sorted()
                 .collect(Collectors.toList());
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
 
-        if(input.isEmpty()) return "Type in a valid Sequence";
-        Set<Integer> uniqueNumberSequence = input
+        
+
+        // if(input.isEmpty()) return "Type in a valid Sequence";
+        List<Integer> uniqueNumberSequence = input
             .stream()
-            .collect(Collectors.toCollection(HashSet::new));
-        int[] sequence = new int[uniqueNumberSequence.size()];
-        int i= 0;
-        for(Integer num: uniqueNumberSequence){
-            if(i < sequence.length)
-            sequence[i++] = num;
-        }
+            .collect(Collectors.toCollection(ArrayList::new));
+        int[] sequence = uniqueNumberSequence.stream().mapToInt(Integer::intValue).toArray();
+        // int[] sequence = new int[uniqueNumberSequence.size()];
+        // int i= 0;
+        // for(Integer num: uniqueNumberSequence){
+        //     if(i < sequence.length)
+        //     sequence[i++] = num;
+        // }
         Arrays.sort(sequence);
         int j = 0, k = 0;
         String buildSequenceString = "";
